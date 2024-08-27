29c66689b66a58bcb18c99f8f21bf17bb3c79633
Eneias Ramos de Melo <eneias@gamuza.com.br>
2024-08-27 16:38:25 -0300

    Added install.sh

---
+++ install.sh
@@ -0,0 +1,21 @@
+#!/bin/bash -e
+
+install()
+{
+    wget 'https://keyserver.ubuntu.com/pks/lookup?op=get&search=0x869689FE09306074' \
+        -O '/etc/apt/trusted.gpg.d/phd-chromium.asc'
+
+    echo "deb https://freeshell.de/phd/chromium/$(lsb_release -sc) /" \
+        | tee /etc/apt/sources.list.d/phd-chromium.list
+
+    apt-get update
+
+    apt-get remove chromium-browser
+
+    apt-get install chromium
+}
+
+export -f install
+
+exec su root -c 'install'
+
